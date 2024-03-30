7183e4d30eb6360df1ec2de2a55de15577d87797
Shaikh-Umar-Farooq <ushaikh_be21@thapar.edu>
2024-03-31 00:11:20 +0530

    awe

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ const PORT = process.env.PORT || 3000;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+mongoose.connect(process.env.MONGO_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+console.log('MongoDB connected');
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+console.error(error.message);
+process.exit(1);
+
   }
-}
+};
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
